40872752d970a6678dd7dda1c34559a7437ee637
PedroAQCoutinho <paq.coutinho@gmail.com>
2024-11-01 12:50:05 -0300

    euler

---
+++ submeter.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#PBS -N exemplo
+#PBS -l select=1:ncpus=40
+#PBS -l walltime=96:00:00
+#PBS -q atlas
+
+# Carrega o módulo de uma versão mais recente do Python
+module load gcc python/3.9.17 # Certifique-se de que essa versão está disponível no sistema
+
+
+# Define o PYTHONPATH para incluir o diretório de instalação local do pandas
+export PYTHONPATH=/mnt/nfs/home/pquilici/.local/lib/python3.9/site-packages:$PYTHONPATH
+
+
+# Muda para o diretório de trabalho
+cd /home/pquilici/gitworkspace/split_offdb
+
+# Executa o script Python e redireciona a saída para um arquivo
+python3 main.py > script.log 2>&1
+
+
