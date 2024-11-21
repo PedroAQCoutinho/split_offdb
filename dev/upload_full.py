231a001ea964ec3dadadbcf002271e7444d1c92a
Pedro Alves Quilici Coutinho <paq.coutinho@gmail.com>
2024-11-21 15:14:55 -0300

    commit

--- dev/upload_full.py
+++ dev/upload_full.py
@@ -36,4 +36,24 @@ f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 )
 
 
+#criar schema e tabela
+create_query=f"CREATE SCHEMA IF NOTE EXISTS split;CREATE TABLE IF NOT EXISTS split.{config['arquivos_final']} (id integer, id_layer text[], id_feature integer[], geometry geometry(polygon, 4674))"
+with engine.connect() as conn:
+    with conn.begin():
+        r=conn.execute(text(create_query))
+
+
+start_time=time.time()
 upload_full_folder(engine=engine, folder=config['output_path'], table_name=config['arquivos_final'])
+print(f'Elapsed time para upload {(time.time() - start_time):.2s}')
+
+queries=['create index if not exists on split.split (id);',
+         'create index if not exists on split.split (id_layer);',
+         'create index if not exists on split.split using gist (geom);',
+         'create index if not exists on split.split (id_feature);']
+
+
+with engine.connect() as conn:
+    for q in queries:
+        with conn.begin():
+            r=conn.execute(text(q))
